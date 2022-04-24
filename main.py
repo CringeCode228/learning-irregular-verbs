155f4407ec95d7d16369b48cbf8221dadd70befb
Artemuy <bibikonfikus1234@gmail.com>
2022-04-24 18:04:29 +0300

    ready

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import sqlite3
+import random
 
 
 def get_random_word():
-    with open("")
+    with sqlite3.connect("database.db") as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT count(1) FROM verbs")
+        count = cursor.fetchone()
+        r = random.randint(1, count[0])
+        cursor.execute(f"SELECT infinitive, past_simple, past_participle, translation FROM verbs WHERE rowid = {r}")
+        return cursor.fetchone()
+
+
+print("Print all forms separated by space:")
+while True:
+    word = get_random_word()
+    print(word[3] + ":")
+    forms = input().split()
+    flag = True
+    if len(forms) == 3:
+        for i, j in zip(word[:3], forms):
+            if i != j:
+                flag = False
+    else:
+        flag = False
+    if flag:
+        print("Yes")
+    else:
+        print("No")
--- parser.py
+++ parser.py
@@ -1,8 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
 
-site = "https://www.ph4.ru/eng_irregular.php?ts=&al=a"
+with sqlite3.connect("database.db") as db:
+    cursor = db.cursor()
+    cursor.execute("SELECT count(1) FROM verbs")
+    if cursor.fetchone()[0] == 0:
+        url = "https://www.ph4.ru/eng_irregular.php?ts=&al=a"
 
-requests.get(site)
-parser = BeautifulSoup()
+        page = requests.get(url)
+        parser = BeautifulSoup(page.text, "html.parser")
+        soup = parser.find_all("td")
+        translations = parser.find_all("td", {"class": "silver"})
+
+        i = 0
+        for index, tag in enumerate(soup[52:-10]):
+            count = tag.find_all("b")
+            if len(count):
+                if len(count[0].text) > 1:
+                    infinitive = count[0]
+                    past_simple = soup[index + 53].find_all("b")
+                    past_participle = soup[index + 54].find_all("b")
+                    if len(past_participle) and len(past_simple):
+                        cursor.execute(f"INSERT INTO verbs (infinitive, past_simple, past_participle, translation) VALUES "
+                                       f"('{infinitive.text}', '{past_simple[0].text}', '{past_participle[0].text}', "
+                                       f"'{translations[i * 2 + 1].text}');")
+                        i += 1
+        db.commit()
